162b0e60d6e5708963688c34af21aa7029c8e90c
DevanshTiwari <devanshtiwari2426@gmail.com>
2026-05-26 18:59:31 +0530

    Added hooks test

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,10 @@ output "lb_ip" {
   value = module.loadbalancer.load_balancer_ip
 }
 
+output "test" {
+  value = "pipeline test"
+}
+
 # output "bucket_url" {
 #   value = data.google_storage_bucket.shared_bucket.url
 # }
