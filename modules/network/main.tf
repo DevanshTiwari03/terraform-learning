d347519a13699b6ef21bdb657ef207910d2ead8c
DevanshTiwari <devanshtiwari2426@gmail.com>
2026-05-26 20:04:33 +0530

    Fixed tfsec findings last

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,30 +1,77 @@
 resource "google_compute_network" "vpc" {
+
   name                    = var.vpc_name
   auto_create_subnetworks = false
 }
 
+
 resource "google_compute_subnetwork" "subnet" {
+
   name          = var.subnet_name
   region        = var.region
   network       = google_compute_network.vpc.id
   ip_cidr_range = "10.10.1.0/24"
+
   log_config {
+
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
     metadata             = "INCLUDE_ALL_METADATA"
   }
 }
 
+
+resource "google_compute_firewall" "allow-ssh" {
+
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+
+    protocol = "tcp"
+    ports     = ["22"]
+  }
+
+  source_ranges = [
+    "203.170.48.2/32"
+  ]
+}
+
+
 resource "google_compute_firewall" "allow-http" {
+
   name    = "allow-http"
   network = google_compute_network.vpc.name
+
   allow {
+
     protocol = "tcp"
-    ports    = ["80"]
+    ports     = ["80"]
   }
-  source_ranges = ["203.170.48.2/32",
+
+  source_ranges = [
+    "0.0.0.0/0"
+  ]
+
+  target_tags = ["web"]
+}
+
+
+resource "google_compute_firewall" "allow-health-checks" {
+
+  name    = "allow-health-checks"
+  network = google_compute_network.vpc.name
+
+  allow {
+
+    protocol = "tcp"
+    ports     = ["80"]
+  }
+
+  source_ranges = [
     "35.191.0.0/16",
     "130.211.0.0/22"
   ]
+
   target_tags = ["web"]
 }
